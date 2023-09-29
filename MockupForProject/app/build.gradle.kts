7e3ef35d9022376204711a1ce0935d5a8dfcd23e
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-09-29 19:50:28 +0200

    Ksp

--- MockupForProject/app/build.gradle.kts
+++ MockupForProject/app/build.gradle.kts
@@ -76,9 +76,32 @@ dependencies {
     // CSV READER
     implementation("com.opencsv:opencsv:5.5.2")
 
-
+    //ksp(project(":test-processor"))
     // ROOM
-    val roomVersion = "2.5.2"
-    implementation("androidx.room:room-runtime:$roomVersion")
-    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    val room_version = "2.5.2"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // To use Kotlin Symbol Processing (KSP)
+    //ksp("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // optional - RxJava2 support for Room
+    implementation("androidx.room:room-rxjava2:$room_version")
+
+    // optional - RxJava3 support for Room
+    implementation("androidx.room:room-rxjava3:$room_version")
+
+    // optional - Guava support for Room, including Optional and ListenableFuture
+    implementation("androidx.room:room-guava:$room_version")
+
+    // optional - Test helpers
+    testImplementation("androidx.room:room-testing:$room_version")
+
+    // optional - Paging 3 Integration
+    implementation("androidx.room:room-paging:$room_version")
+
 }
