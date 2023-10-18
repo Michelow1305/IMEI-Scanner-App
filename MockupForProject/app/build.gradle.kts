e251939872eb6c761f9b147aa57b055736f0466a
Michel <micheljensen03@gmail.com>
2023-10-18 16:39:21 +0200

    mlkit text recognition up and running :D

--- MockupForProject/app/build.gradle.kts
+++ MockupForProject/app/build.gradle.kts
@@ -51,6 +51,7 @@ android {
 }
 
 dependencies {
+    implementation("com.google.mlkit:text-recognition:16.0.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
--- MockupForProject/app/src/main/java/com/hkr/mockupforproject/ImeiCodeAnalyzer.kt
+++ MockupForProject/app/src/main/java/com/hkr/mockupforproject/ImeiCodeAnalyzer.kt
@@ -9,18 +9,21 @@ import androidx.compose.runtime.State
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
 
 class ImeiCodeAnalyzer(
     private val onImeiCodesDetected: (imeis: List<Long>) -> Unit,
     private val rect: State<Rect>
 ) : ImageAnalysis.Analyzer {
-
     private val imeiLength = 15
-    private val tag = "analyzer"
     private val scanner = BarcodeScanning.getClient(
         BarcodeScannerOptions.Builder()
             .enableAllPotentialBarcodes()
             .build()
+    )
+    private val textscanner = TextRecognition.getClient(
+        TextRecognizerOptions.DEFAULT_OPTIONS
     )
 
     @androidx.annotation.OptIn(ExperimentalGetImage::class)
@@ -36,42 +39,59 @@ class ImeiCodeAnalyzer(
         adjustedRect.right = rect.right
         adjustedRect.bottom = rect.top
 
+
         scanner.process(image)
             .addOnSuccessListener { barcodes ->
                 val imeis = barcodes.mapNotNull { barcode ->
-
                     if (barcode.boundingBox?.let { adjustedRect.contains(it) } == true) {
                         try {
-
                             barcode.rawValue?.takeIf {
                                 it.isNotBlank() && it.length == imeiLength && it.all { char -> char.isDigit() }
                             }?.toLong()
-
                         } catch (ex: NumberFormatException) {
                             null
-
                         }
-
-
                     } else {
                         null
-
                     }
-
                 }
-
                 onImeiCodesDetected(imeis)
-
             }
-
-            .addOnFailureListener {
-                Log.d(tag, "Analysis Failed")
-            }
-
             .addOnCompleteListener {
                 imageProxy.close()
             }
-
+        textscanner.process(image)
+            .addOnSuccessListener {text ->
+                for (block in text.textBlocks) {
+                    for (line in block.lines) {
+                        val lineText = line.text
+                        if(lineText.length == imeiLength) {
+                            var imeis: List<Long> = listOf()
+                            try {
+                                imeis = listOf(lineText.toLong())
+                            } catch (e : NumberFormatException) { null }
+                            if(imeis.isNotEmpty()) {
+                                onImeiCodesDetected(imeis)
+                            } else null
+                        }
+                        // I leave all these examples in here in case we want to work with it later
+                        /*
+                        val blockText = block.text
+                        val blockCornerPoints = block.cornerPoints
+                        val blockFrame = block.boundingBox
+                        val lineCornerPoints = line.cornerPoints
+                        val lineFrame = line.boundingBox
+                        for (element in line.elements) {
+                            val elementText = element.text
+                            val elementCornerPoints = element.cornerPoints
+                            val elementFrame = element.boundingBox
+                        }
+                        */
+                    }
+                }
+            }
+            .addOnCompleteListener {
+                imageProxy.close()
+            }
     }
-
 }
