b109163c55573ab0ab239ecd9b380348a74b9eb5
Sathwik <96546441+sathwikkannam@users.noreply.github.com>
2023-10-19 12:59:55 +0200

    Vibration, colors
    Vibrate on detecting IMEIs. Change colors to MaterialTheme. Add tooltip to strings.xml. Show toasts instead of throwing securityException

--- MockupForProject/app/src/main/java/com/hkr/mockupforproject/ui/screens/Camera.kt
+++ MockupForProject/app/src/main/java/com/hkr/mockupforproject/ui/screens/Camera.kt
@@ -3,7 +3,12 @@ package com.hkr.mockupforproject.ui.screens
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Rect
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
@@ -59,6 +64,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.layout.positionInParent
 import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -75,6 +81,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.navigation.NavController
 import com.google.common.util.concurrent.ListenableFuture
 import com.hkr.mockupforproject.ImeiCodeAnalyzer
+import com.hkr.mockupforproject.R
 import com.hkr.mockupforproject.ui.AppViewModel
 import kotlinx.coroutines.launch
 
@@ -88,10 +95,11 @@ import kotlinx.coroutines.launch
  * @param context to bind the CameraX lifecycle to activity's lifecycle.
  * @param appViewModel to add the scanned IMEI's to a MutableLiveData list in the view model.
  * @param mainNavController to navigate to another screen/composable
- * @throws SecurityException if camera permission is not granted.
+ * @requires Build.VERSION_CODES.O to vibrate the phone.
  * @suppress UnusedMaterial3ScaffoldPaddingParameter setting padding to the scaffold screws with the
  *                                                   .onGloballyPositioned.
  */
+@RequiresApi(Build.VERSION_CODES.O)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun CameraWithBoundedBox(
@@ -101,9 +109,10 @@ fun CameraWithBoundedBox(
     mainNavController: NavController
 ) {
     if (!appViewModel.hasCameraPermission(context)) {
-        throw SecurityException("Camera permission not granted")
+        Toast.makeText(context, "Camera permission not granted", Toast.LENGTH_LONG).show()
+        return
     }
-    var tempImei = appViewModel.currentDeviceToSave.imei
+
     val imeis by appViewModel.scannedImeis.observeAsState(emptyList())
     /*
         I added cameraProviderFuture outside the CameraPreview(), so I can unbind it, whenever the user
@@ -128,6 +137,8 @@ fun CameraWithBoundedBox(
                     cameraProviderFuture.get().unbindAll()
                     mainNavController.popBackStack()
                 },
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.secondary,
                 expanded = false,
                 icon = { Icon(Icons.Filled.KeyboardArrowLeft, null) },
                 text = { Text(text = "") },
@@ -146,9 +157,9 @@ fun CameraWithBoundedBox(
                         .align(Alignment.CenterHorizontally),
                     reverseLayout = true,
                 ) {
-                    if(appViewModel.currentDeviceToSave.imei != tempImei){
-                        tempImei = appViewModel.currentDeviceToSave.imei
-                        items(listOf(tempImei)) { imei ->
+                    if (imeis.isNotEmpty()) {
+                        vibrate(context)
+                        items(listOf(imeis.last())) { imei ->
                             Imei(imei = imei)
                         }
                     }
@@ -166,8 +177,8 @@ fun CameraWithBoundedBox(
                     expanded = true,
                     icon = { Icon(Icons.Filled.Done, null) },
                     text = { Text(text = "Done") },
-                    containerColor = if (imeis.isNotEmpty()) MaterialTheme.colorScheme.primary else Color.Gray,
-                    contentColor = if (imeis.isNotEmpty()) Color.White else Color.DarkGray
+                    containerColor = if (imeis.isNotEmpty()) MaterialTheme.colorScheme.primaryContainer else Color.Gray,
+                    contentColor = if (imeis.isNotEmpty()) MaterialTheme.colorScheme.secondary else Color.DarkGray
                 )
                 Text(
                     text = "Enter manually",
@@ -346,13 +357,13 @@ private fun CameraPreview(
         .build()
 
     if (!cameraProvider.hasCamera(cameraSelector)) {
-        throw SecurityException("Phone has no back camera.")
+        Toast.makeText(context, "Phone has no back camera.", Toast.LENGTH_LONG).show()
+        return
     }
 
     AndroidView(
         modifier = modifier
-            .fillMaxSize()
-        ,
+            .fillMaxSize(),
         factory = { ctx ->
             PreviewView(ctx).apply {
                 implementationMode = PreviewView.ImplementationMode.COMPATIBLE
@@ -383,7 +394,7 @@ private fun CameraPreview(
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ScanImeiTooltip(modifier: Modifier = Modifier){
+fun ScanImeiTooltip(modifier: Modifier = Modifier) {
     val tooltipState = rememberRichTooltipState(isPersistent = true)
     val scope = rememberCoroutineScope()
 
@@ -391,15 +402,21 @@ fun ScanImeiTooltip(modifier: Modifier = Modifier){
         append("Scan IMEI code. ")
 
         pushStringAnnotation(tag = "learn more", annotation = "")
-        withStyle(style = SpanStyle(color = MaterialTheme.colorScheme.primary, textDecoration = TextDecoration.Underline)) {
+        withStyle(
+            style = SpanStyle(
+                color = MaterialTheme.colorScheme.primary,
+                textDecoration = TextDecoration.Underline
+            )
+        ) {
             append("Learn more")
         }
     }
     RichTooltipBox(
         text = {
 
-            Text(text = "An IMEI barcode is a 15-digit code that is used to identify a device. " +
-                    "It is usually found on the back of the device.")
+            Text(
+                text = stringResource(R.string.scan_imei_tooltip)
+            )
         },
         title = { Text(text = "Scanning IMEI barcodes") },
         action = {
@@ -414,17 +431,21 @@ fun ScanImeiTooltip(modifier: Modifier = Modifier){
             }
         },
         tooltipState = tooltipState,
-        modifier = modifier
-            .padding(top=200.dp, bottom = 170.dp),
+//        modifier = modifier
+//            .padding(top = 200.dp, bottom = 170.dp),
     ) {
         Box(
             modifier = modifier
-        ){
+        ) {
 
             ClickableText(
                 text = annotatedString,
                 onClick = { offset ->
-                    annotatedString.getStringAnnotations(tag = "learn more", start = offset, end = offset).firstOrNull()?.let {
+                    annotatedString.getStringAnnotations(
+                        tag = "learn more",
+                        start = offset,
+                        end = offset
+                    ).firstOrNull()?.let {
                         scope.launch { tooltipState.show() }
                     }
                 },
@@ -443,6 +464,13 @@ fun ScanImeiTooltip(modifier: Modifier = Modifier){
     }
 }
 
+
+@RequiresApi(Build.VERSION_CODES.O)
+private fun vibrate(context: Context) {
+    val vibrator = context.getSystemService(Vibrator::class.java)
+
+    vibrator?.vibrate(VibrationEffect.createOneShot(250, VibrationEffect.DEFAULT_AMPLITUDE))
+}
 
 /**
  * This adds border to only the corners.
